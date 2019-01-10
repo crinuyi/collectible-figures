b7a310c40afe9a7b6c6c65e3f4a56016e2fd4a82
crin <crinuyi@gmail.com>
2019-01-10 23:25:36 +0100

    added statistics (viewmodel)

---
+++ collectible-figures/Controllers/StatisticsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using collectible_figures.Models;
+using collectible_figures.ViewModels;
+
+namespace collectible_figures.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Statistics
+        [Route("statystyki")]
+        public ActionResult Index()
+        {
+            var model = new StatisticsViewModel();
+
+            model.amountOfFigures = db.Figures.Count();
+            model.amountOfClassifications = db.Classifications.Count();
+            model.amountOfSeries = db.Series.Count();
+
+            return View(model);
+        }
+    }
+}
