92d6b72d151c2211f126edc9143b0c377a3a5dbd
crin <37940924+crinuyi@users.noreply.github.com>
2019-04-25 19:46:04 +0200

    added methods

--- collectible-figures/Database/DatabaseContext.cs
+++ collectible-figures/Database/DatabaseContext.cs
@@ -2,8 +2,91 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using collectible_figures.Models;
 
 namespace collectible_figures.Database {
-    public class DatabaseContext {
+    public class DatabaseContext : IDatabaseContext {
+        Dictionary<int, Figure> Figures { get; set; }
+        Dictionary<int, Series> Series { get; set; }
+        Dictionary<int, Classification> Classifications { get; set; }
+        IQueryable<Figure> IDatabaseContext.Figures {
+            get { return Figures.Select(x => x.Value).AsQueryable<Figure>(); }
+        }
+        IQueryable<Classification> IDatabaseContext.Classifications {
+            get { return Classifications.Select(x => x.Value).AsQueryable<Classification>(); }
+        }
+
+        IQueryable<Series> IDatabaseContext.Series {
+            get { return Series.Select(x => x.Value).AsQueryable<Series>(); }
+        }
+
+        T IDatabaseContext.Add<T>(T entity) {
+            if (entity is Figure) {
+                Figure figure = entity as Figure;
+                Figures.Add(figure.FigureID, figure);
+                return entity;
+            }
+            else if (entity is Classification) {
+                Classification classification = entity as Classification;
+                Classifications.Add(classification.ClassificationID, classification);
+                return entity;
+            }
+            else if (entity is Series) {
+                Series series = entity as Series;
+                Series.Add(series.SeriesID, series);
+                return entity;
+            }
+            else throw new TypeLoadException();
+        }
+
+        T IDatabaseContext.Delete<T>(T entity) {
+            if (entity is Figure) {
+                Figure figure = entity as Figure;
+                if (Figures.ContainsKey(figure.FigureID)) {
+                    Figures.Remove(figure.FigureID);
+                    return entity;
+                }
+                else throw new KeyNotFoundException();
+            }
+            else if (entity is Classification) {
+                Classification classification = entity as Classification;
+                if (Classifications.ContainsKey(classification.ClassificationID)) {
+                    Classifications.Remove(classification.ClassificationID);
+                    return entity;
+                }
+                else throw new KeyNotFoundException();
+            }
+            else if (entity is Series) {
+                Series series = entity as Series;
+                if (Series.ContainsKey(series.SeriesID)) {
+                    Series.Remove(series.SeriesID);
+                    return entity;
+                }
+                else throw new KeyNotFoundException();
+            }
+            else throw new TypeLoadException();
+        }
+
+        Classification IDatabaseContext.FindClassificationById(int id) {
+            if (Classifications.ContainsKey(id))
+                return Classifications[id];
+            else throw new KeyNotFoundException();
+        }
+
+        Figure IDatabaseContext.FindFigureById(int id) {
+            if (Figures.ContainsKey(id))
+                return Figures[id];
+            else throw new KeyNotFoundException();
+        }
+
+        Series IDatabaseContext.FindSeriesById(int id) {
+            if (Series.ContainsKey(id))
+                return Series[id];
+            else throw new KeyNotFoundException();
+        }
+
+        int IDatabaseContext.SaveChanges() {
+            return 0;
+        }
     }
 }
--- collectible-figures/Database/IDatabaseContext.cs
+++ collectible-figures/Database/IDatabaseContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using collectible_figures.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +7,15 @@ using System.Threading.Tasks;
 
 namespace collectible_figures.Database {
     interface IDatabaseContext {
+        IQueryable<Figure> Figures { get; }
+        IQueryable<Classification> Classifications { get; }
+        IQueryable<Series> Series { get; }
+        int SaveChanges();
+        T Add<T>(T entity) where T : class;
+        Figure FindFigureById(int id);
+        Classification FindClassificationById(int id);
+        Series FindSeriesById(int id);
+        T Delete<T>(T entity) where T : class;
+        
     }
 }
