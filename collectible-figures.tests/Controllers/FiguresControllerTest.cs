b26bc712ac94940ed794009c33f5462e20fc08ba
crin <37940924+crinuyi@users.noreply.github.com>
2019-04-25 22:46:05 +0200

    added tests
    FiguresController + SeriesController (unfinished)

---
+++ collectible-figures.tests/Controllers/FiguresControllerTest.cs
@@ -0,0 +1,32 @@
+﻿using collectible_figures.Controllers;
+using collectible_figures.Database;
+using collectible_figures.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace collectible_figures.tests.Controllers {
+    [TestClass]
+    class FiguresControllerTest {
+        IDatabaseContext databaseContext;
+        FiguresController figuresController;
+        Figure figure;
+
+        [TestInitialize]
+        public void init() {
+            databaseContext = new DatabaseContext();
+            figuresController = new FiguresController(databaseContext);
+
+            figure = new Figure();
+            figure.FigureID = 1;
+            figure.Name = "Sample figure";
+            figure.Scale = "1:30";
+            figure.ReleaseDate = new DateTime(2019, 10, 10);
+            figure.Price = 300;
+        }
+
+    }
+}
---
+++ collectible-figures.tests/Controllers/SeriesControllerTest.cs
@@ -0,0 +1,56 @@
+﻿using collectible_figures.Controllers;
+using collectible_figures.Database;
+using collectible_figures.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace collectible_figures.tests.Controllers {
+    [TestClass]
+    public class SeriesControllerTest {
+        IDatabaseContext databaseContext;
+        SeriesController seriesController;
+        Series series;
+
+        [TestInitialize]
+        public void init() {
+            databaseContext = new DatabaseContext();
+            seriesController = new SeriesController(databaseContext);
+            series = new Series();
+            series.SeriesID = 1;
+            series.Name = "Sample name";
+        }
+
+        [TestMethod]
+        public void IndexTest() {
+            ViewResult viewResult = seriesController.Index() as ViewResult;
+            var model = viewResult.Model;
+
+            Assert.IsNotNull(viewResult);
+            Assert.IsNotNull(model);
+            Assert.IsTrue(model is IList<Series>);
+        }
+
+        [TestMethod]
+        public void DetailsTest() {
+            databaseContext.Add(series);
+            ViewResult viewResult = seriesController.Details(1) as ViewResult;
+            var model = viewResult.Model;
+
+            Assert.IsNotNull(viewResult);
+            Assert.IsNotNull(model);
+            Assert.IsTrue(model is Series);
+        }
+
+        [TestCleanup]
+        public void cleanup() {
+            databaseContext = null;
+            seriesController = null;
+            series = null;
+        }
+    }
+}
