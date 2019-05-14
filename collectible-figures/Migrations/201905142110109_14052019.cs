53dd851539fcb4c4998cd9d8d37a0a5b2993d5db
crin <37940924+crinuyi@users.noreply.github.com>
2019-05-15 00:16:53 +0200

    added migration

---
+++ collectible-figures/Migrations/201905142110109_14052019.cs
@@ -0,0 +1,16 @@
+namespace collectible_figures.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _14052019 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
